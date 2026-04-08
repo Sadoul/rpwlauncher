e713843dab399bd940bc7c3225882b134874da22
Sadoul <sadoul@rpworld.com>
2026-04-08 11:53:36 +0300

    chore: add stub/target to gitignore

--- stub/target/release/deps/RPWorld_Launcher.d
+++
@@ -1,5 +0,0 @@
-C:\Users\smopo\Desktop\RPWLauncher\stub\target\release\deps\RPWorld_Launcher.d: src\main.rs
-
-C:\Users\smopo\Desktop\RPWLauncher\stub\target\release\deps\RPWorld_Launcher.exe: src\main.rs
-
-src\main.rs:
